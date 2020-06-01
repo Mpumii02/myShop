974881e7a85dfa864615ed20223c0cf5a56bd525
Katleho <mokoena.mpumi05@gmail.com>
2020-06-01 17:55:40 +0200

    Created the Product Filter

--- myShop/myShop.WebUI/Controllers/HomeController.cs
+++ myShop/myShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using myShop.Core.Contracts;
 using myShop.Core.Models;
+using myShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,26 @@ namespace myShop.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<productCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+
+            return View(model);
         }
 
 
